fa0f75e78ca4e190e7614564417f1563a706b639
Gags-21 <gagan.bhirangi123@gmail.com>
2022-09-16 22:05:03 +0530

    Initial Commit

---
+++ Shared/flutter_counter_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_counter_IOSApp.swift
+//  Shared
+//
+//  Created by Gagan Bhirangi on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct flutter_counter_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
