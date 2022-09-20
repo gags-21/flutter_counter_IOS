15175d11d791752e0de22fba5d53ba3648c4a99e
Gags-21 <gagan.bhirangi123@gmail.com>
2022-09-20 22:15:02 +0530

    Implementing Navigation Bar

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,34 +10,53 @@ import SwiftUI
 struct ContentView: View {
     
     @State var count = 0
-
-var body: some View {
+    
+    var body: some View {
         
-        ZStack{
+//        NavigationView{
             
-            VStack{
+            ZStack{
                 
-                Text("You have pushed the button this many times:").padding()
-                Text(String(count)).padding()
+                VStack{
+                    
+                    Text("You have pushed the button this many times:").padding()
+                    Text(String(count)).padding()
+                    
+                }
+                extractedFunc()
                 
             }
-            extractedFunc()
+            
+//            .navigationTitle("Basic app")
+            
+//        }
+                .overlay {
+                            ZStack {
+                                Color.clear
+                                    .background(.ultraThinMaterial)
         
+                                Text("Navigation Bar")
+                                    .font(.largeTitle.weight(.bold))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.leading, 20)
+                            }
+                            .frame(height: 70)
+                            .frame(maxHeight: .infinity, alignment: .top)
+                        }
     }
-}
-
-
-func extractedFunc() -> some View {
-   Button {
-       count += 1
-   } label: {
-       Text("+")
-   }
-   .tint(.blue)
-   .buttonStyle(.borderedProminent)
-   .buttonBorderShape(.roundedRectangle(radius: 50))
-   .controlSize(.large)
-   .position(x: 380, y: 800)
+    
+    
+    func extractedFunc() -> some View {
+        Button {
+            count += 1
+        } label: {
+            Text("+")
+        }
+        .tint(.blue)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.roundedRectangle(radius: 50))
+        .controlSize(.large)
+        .position(x: 380, y: 800)
     }
 }
 
